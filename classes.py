c423eb5d8b6fbce92f62e73e1ec8edfc44bc9ad9
Artifice-219 <johnphilliplormalbas@gmail.com>
2025-01-21 02:46:19 -0800

    board now accepts input and can validate if the inputs are valid

--- classes.py
+++ classes.py
@@ -5,6 +5,24 @@ class Board:
             ['_','_', '_'],
             ['_','_', '_']
         ]
+    # just displays the current board, this method musn't "re-draw" the board
+    def get_board(self):
+        rows = len(self.board)
+        col = len(self.board)
+
+        for r in range(rows):
+            print(self.board[r][0], '|', self.board[r][1], '|', self.board[r][2],)
+
+    # put() put chars in the specified coordinate
+    def put(self, row, col, sym):
+        # check if the sym is an alphabetic character
+        if(sym.isalpha()):
+            sym = sym.lower()
+            # symbol validation
+            if sym == 'x' or  sym == 'o':
+                self.board[row][col] = sym
+        else:
+            print('Please choose only between X and O ')
 
     def make_board(self):
         # do some formatting here to print the board prettily
--- main.py
+++ main.py
@@ -2,4 +2,10 @@ from classes import *
 
 board = Board()
 
-board.make_board()
+
+board.put(1,1, 'X')
+board.get_board()
+board.put(0,0, 'O')
+board.get_board()
+board.put(2, 2, '5')
+board.get_board()
