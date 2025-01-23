741b94a5115f083c6d5f5abe6dfd0cd8b2ecc00d
Artifice-219 <johnphilliplormalbas@gmail.com>
2025-01-23 02:54:53 -0800

    board can now check for winning condition

--- classes.py
+++ classes.py
@@ -16,13 +16,17 @@ class Board:
     # put() put chars in the specified coordinate
     def put(self, row, col, sym):
         # check if the sym is an alphabetic character
-        if(sym.isalpha()):
-            sym = sym.lower()
-            # symbol validation
-            if sym == 'x' or  sym == 'o':
-                self.board[row][col] = sym
+        # check if the cell at that location is free
+        if(self.board[row][col] == '_'):
+            if(sym.isalpha()):
+                sym = sym.lower()
+                # symbol validation
+                if sym == 'x' or  sym == 'o':
+                    self.board[row][col] = sym
+            else:
+                print('Please choose only between X and O ')
         else:
-            print('Please choose only between X and O ')
+            print(f'Board at {row} - {col} is not free')
 
     def make_board(self):
         # do some formatting here to print the board prettily
@@ -31,4 +35,45 @@ class Board:
 
         for r in range(rows):
             print(self.board[r][0], '|', self.board[r][1], '|', self.board[r][2],)
+    
+    def horizontal_win(self, sym):
+         # check for horizontal win
+        count = 0
+        for row in range(3):
+            for col in range(3):
+                # counts how many times does the symbol sym happen consecutively
+                if(self.board[row][col] == sym):
+                    count += 1
+                # if the repetition is not repeating revert count to 0
+                else:
+                    count = 0
+                if(count == 3):
+                    return print('A Horizontal winning condition is found')
+
+    def vertical_win(self, sym):
+        # check for vertical win
+        count = 0
+        for row in range(3):
+            for col in range(3):
+                if(self.board[col][row] == sym):
+                    count += 1
+                # if the repetition is not repeating revert count to 0
+                else:
+                    count = 0
+                if(count == 3):
+                    return print('A Vertical winning condition is found')
+    
+    def diagonal_win(self, sym):
+        # check for winning diagonal condition is sadly hardcoded
+        count = 0
+        if(self.board[0][0] and self.board[1][1] and  self.board[2][2]  == sym):
+            return print('A diagonal winning condition is found')
         
+        if(self.board[0][2] and self.board[1][1] and  self.board[2][0]  == sym):
+            return print('A diagonal winning condition is found')
+    
+    # consolidate all functions that check for winning conditions
+    def is_solved(self, sym):
+        if(self.horizontal_win(sym) or self.vertical_win(sym) or self.diagonal_win(sym)):
+            return True
+        return False
--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from classes import *
 board = Board()
 
 
-board.put(1,1, 'X')
+board.put(0,1, 'x')
+# board.get_board()
+board.put(1,1, 'x')
+# board.get_board()
+board.put(2, 1, 'x')
+# board.get_board()
 board.get_board()
-board.put(0,0, 'O')
-board.get_board()
-board.put(2, 2, '5')
-board.get_board()
+board.is_solved('x')
+
+
+def game_loop():
+    print('Game is running')
+
+
+# simulate continous game running
+for i in range(10):
+    game_loop()
